2b481bc0b3f69fce604aa67bc5ffd73d1fb612ef
Touiyake <touiyake@gmail.com>
2019-07-05 18:20:59 +0800

    deleted unnecessary files

--- spring-template/src/main/java/com/spring/template/layer/dao/UserDao.java
+++
@@ -1,9 +0,0 @@
-package com.spring.template.layer.dao;
-
-import java.io.Serializable;
-
-import com.spring.template.abstracts.BasicOperations;
-
-public interface UserDao<T extends Serializable> extends BasicOperations<T> {
-
-}
--- spring-template/src/main/java/com/spring/template/layer/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.spring.template.layer.service;
-
-import java.io.Serializable;
-
-import com.spring.template.abstracts.BasicOperations;
-
-public interface UserService<T extends Serializable> extends BasicOperations<T> {
-
-}
